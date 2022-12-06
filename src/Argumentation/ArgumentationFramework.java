401b78e503fce6637f50ba0e60f2d16f4ac5fd1d
Hugo Demaret <hugovladimir2000@gmail.com>
2022-12-06 13:24:16 +0100

    Added {     ArgumentationSet     Type : Class (protected)     Objective : TreeSet of arguments, implements Comparable     ------     isPrefered     Type : method (public)     Signature : void -> boolean     Objective : returns true if set is prefered, false otherwise     ------     getSubset     Type : method (public)     Signature : void -> TreeSet<ArgumentSet>     Objective : computes all the subsets of this     ------     constructAdmissible     Type : method (public)     Signature : void -> void     Objective : constructs all the admissible solutions (possible none) and put them into admissibleSet     ------     constructPrefered     Type : method (public)     Signature : void -> void     Objective : constructs all the prefered solutions (possibly none) and put them into preferedSet } In : ArgumentationFramework.java

--- src/Argumentation/ArgumentationFramework.java
+++ src/Argumentation/ArgumentationFramework.java
@@ -7,6 +7,57 @@ import java.util.*;
 public class ArgumentationFramework {
     private Map<Integer,Argument> argumentSet;
     private Map<Integer,Argument> solutionSet;
+    private Set<Set<Argument>> admissibleSet;
+    private Set<Set<Argument>> preferedSet;
+
+
+    /**
+     * <p>This class extends TreeSet in order to be easier to use, but also faster.
+     * It allows to compare two TreeSets of arguments between one another
+     * It implements a hash value
+     * </p>
+     */
+    protected class ArgumentationSet extends TreeSet<Argument> implements Comparable<ArgumentationSet>{
+
+        private int hash;
+
+        /**
+         * <p>The hash value is the sum of all argument's identifier</p>
+         * @return int : the hash value
+         */
+        public int getHash(){
+            return hash;
+        }
+
+        /**
+         * <p>Adds an argument to ArgumentSet. Increments the hash by the argument's identifie</p>
+         * @param argument
+         * @return
+         */
+        @Override
+        public boolean add(Argument argument) {
+            hash+=argument.getIdentifier();
+            return super.add(argument);
+        }
+
+        /**
+         * <p>Allows for comparison between two TreeSets of Arguments</p>
+         * @param o the argument to be compared to this
+         * @return -1 if this is smaller, 0 if equals, 1 if larger
+         */
+        @Override
+        public int compareTo(ArgumentationSet o) {
+            if (this.size() < o.size())
+                return -1;
+            else if (this.size() > o.size())
+                return 1;
+            if (this.hash < o.hash)
+                return -1;
+            else if (this.hash > o.hash)
+                return 1;
+            else return 0;
+        }
+    }
 
     /**
      * <p>Constructor</p>
@@ -44,7 +95,7 @@ public class ArgumentationFramework {
      * @param b
      */
     public void addContradiction(Argument a, Argument b){
-        if(!(argumentSet.containsKey(a.getIdentifier()) || argumentSet.containsKey(b.getIdentifier()))) {
+        if(!(argumentSet.containsKey(a.getIdentifier()) && argumentSet.containsKey(b.getIdentifier()))) {
             //gestion des exceptions
             System.out.println("Error : arguments not in the set\n");
         } else {
@@ -185,6 +236,32 @@ public class ArgumentationFramework {
        return existsContradict && existsDef;
     }
 
+    public boolean isPrefered(){
+        boolean result = true;
+        return result;
+    }
+
+public Set<Set<Argument>> getSubset(){
+        Set<Set<Argument>> subsets;
+        subsets = new TreeSet<Set<Argument>>();
+    }
+
+    public void constructAdmissible(){
+        for (int i = 0 ; i < (1<<argumentSet.size()); ++i){
+
+        }
+    }
+
+    public void constructPrefered(){
+
+    }
+
+
+    /**
+     *
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -193,6 +270,11 @@ public class ArgumentationFramework {
         return Objects.equals(argumentSet, that.argumentSet) && Objects.equals(solutionSet, that.solutionSet);
     }
 
+
+    /**
+     *
+     * @return
+     */
     @Override
     public int hashCode() {
         return Objects.hash(argumentSet, solutionSet);
